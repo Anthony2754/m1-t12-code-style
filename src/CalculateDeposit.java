3670b9e713870a84ac84eb79b5b54f4509b5184d
Anton Bobrov <anthony020@yandex.ru>
2022-11-24 11:28:23 +0300

    2

--- src/CalculateDeposit.java
+++ src/CalculateDeposit.java
@@ -1,22 +1,22 @@
 import java.util.Scanner;
 
     public class CalculateDeposit {
-        double CalculateComplexPercentFunction(double a, double y, int d) {
+        double calculateComplexPercentFunction(double a, double y, int d) {
 
             double pay = a * Math.pow((1 + y / 12), 12 * d);
-            return Round(pay, 2);
+            return round(pay, 2);
         }
 
-        double CalculateSimplePercentFunction(double doubleAmount, double doubleYearRate, int depositTеrm) {
-            return Round (doubleAmount + doubleAmount * doubleYearRate * depositTеrm, 2);
+        double calculateSimplePercentFunction(double doubleAmount, double doubleYearRate, int depositTеrm) {
+            return round (doubleAmount + doubleAmount * doubleYearRate * depositTеrm, 2);
         }
 
-        double Round(double value,int places) {
+        double round(double value,int places) {
             double ScaLe = Math.pow(10, places);
             return Math.round(value*ScaLe) / ScaLe;
         }
 
-        void DoImportantJob() {
+        void doImportantJob() {
             int period;
             int depositType;
 
@@ -30,18 +30,18 @@ import java.util.Scanner;
             System.out.println("Выберите тип вклада, 1 - вклад с обычным процентом, 2 - вклад с капитализацией:");
             depositType = scanner.nextInt();
 
-            double outDoubleVar = 0;
+            double depositAmount = 0;
 
             if (depositType == 1) {
-                outDoubleVar = CalculateSimplePercentFunction(amount, 0.06, period);
+                depositAmount = calculateSimplePercentFunction(amount, 0.06, period);
             } else if (depositType == 2) {
-                outDoubleVar = CalculateComplexPercentFunction(amount, 0.06, period);
+                depositAmount = calculateComplexPercentFunction(amount, 0.06, period);
             }
-            System.out.println("Результат вклада: " + amount + " за " + period + " лет превратятся в " + outDoubleVar);
+            System.out.println("Результат вклада: " + amount + " за " + period + " лет превратятся в " + depositAmount);
         }
 
         public static void main(String[] args) {
-            new CalculateDeposit().DoImportantJob();
+            new CalculateDeposit().doImportantJob();
         }
     }
 
